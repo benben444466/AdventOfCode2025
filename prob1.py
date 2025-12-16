1c91f94532b4f47a40988a8b7f0111fb07805ec7
benben444466 <pwc123451@gmail.com>
2025-12-16 14:09:27 -0500

    completed part 1 of problem 1

--- prob1.py
+++ prob1.py
@@ -1,22 +1,22 @@
-file_path = "input1.txt"
-with open(file_path, 'r') as file:
-    lines = file.readlines()
 
 START_POINT = 50
+test_path = "test1.txt"
+file_path = "input1.txt"
+with open(test_path, 'r') as file:
+    lines = file.readlines()
 
 when_left_on_zero = []
 current = START_POINT
 for line in lines:
     direction = line[0]
     number = int(''.join(i for i in line if i.isdigit()))
-
+    print(direction, number)
     if direction == 'R':
-        current = (current + number) % 99 
+        if current 
+        current = (current + number) % 100
     elif direction == 'L':
-        current = (current - number) % 99
-    if current == 0:
-        when_left_on_zero.append(line) 
+        current = (current - number) % 100 
+    when_left_on_zero.append((line, current))
 
+print(when_left_on_zero)
 print("result is: ", len(when_left_on_zero))
-
-# Write Test Suite here
